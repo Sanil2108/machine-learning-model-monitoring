f9a4fdc055c933373c52c6e9a2913f53aecdd40f
Sanil <sanilkhurana7@gmail.com>
2021-02-14 22:08:46 +0530

    Minor changes in deployment script

--- main-instance-services/deploy.sh
+++ main-instance-services/deploy.sh
@@ -2,4 +2,4 @@
 ssh -i ../../aws/main-instance.pem ubuntu@3.7.206.61 "sudo apt-get update && sudo apt-get install -y docker.io && sudo apt-get install -y docker-compose"
 scp -i ../../aws/main-instance.pem docker-compose.yml ubuntu@3.7.206.61:/home/ubuntu/
 scp -r -i ../../aws/main-instance.pem database ubuntu@3.7.206.61:/home/ubuntu/
-ssh -i ../../aws/main-instance.pem ubuntu@3.7.206.61 "cd && sudo docker-compose up -d" 
+ssh -i ../../aws/main-instance.pem ubuntu@3.7.206.61 "cd && sudo docker-compose pull && sudo docker-compose kill && sudo docker-compose up -d" 
